a7aa26cadc2ddcb360eccecc5d3a2d446cdec828
anjali <anjalishinde363@gmail.com>
2022-09-26 23:46:36 +0530

    first challenge

---
+++ CafeManagement/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CafeManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuest_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Dashboard ds = new Dashboard("Guest");
+            ds.Show();
+            this.Hide();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == "mscdays" && txtPassword.Text == "pass")
+            {
+                Dashboard ds = new Dashboard("Admin");
+                ds.Show();
+                this.Hide();
+            }
+        }
+    }
+}
